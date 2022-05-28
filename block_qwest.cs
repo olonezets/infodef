9739f709719425447775acf2881237ac5b864882
Anatolii Babkin <olonezets@mail.com>
2022-05-28 23:51:11 +0300

    Add files via upload

---
+++ block_qwest.cs
@@ -0,0 +1,58 @@
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseActive == false)
+            {
+                pausePanel.SetActive(true);
+                pauseActive = true;
+            }
+            else
+            {
+                pausePanel.SetActive(false);
+                pauseActive = false;
+            }
+        }
+        if (debugTggl == 1)
+        {
+            debugText[0].enabled = true;
+            debugText[0].text = "Дебаг мод: Число с нумерацией вопроса = " + wQ.ToString();
+            debugText[1].enabled = true;
+            debugText[1].text = "Кликнутый ответ = " + clickedAnswer.ToString();
+            debugText[2].enabled = true;
+            debugText[2].text = "Правильный ответ = " + trueint.ToString();
+        }
+
+        timeText.text = timeEq.ToString();
+        timeF += Time.deltaTime;
+        if (timeF >= 1.2)
+        {
+            if (clickedAnswer == -1)
+            {
+                if (pauseActive == false)
+                {
+                    timeEq--;
+                    timeF = 0;
+                }
+            }
+        }
+        if (timeEq <= 0)
+        {
+            timeText.text = "Время истекло";
+            overTime();
+        }
+        if (timeEq <= 3)
+        {
+            timeText.color = Color.red;
+        }
+        if (wQ == 1)
+        {
+            RQ1();
+        }
+        
+		else if (wQ == 25)
+        {
+            RQ25();
+        }
+
+    }
